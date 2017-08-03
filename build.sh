9a202d4f4a042870bbb1ad4ece1995eaa17bdea7
José Antônio Rapozo <joseantonio.rapozo@gmail.com>
2017-08-03 14:40:58 -0300

    Travis

--- build.sh
+++ build.sh
@@ -1,17 +1,2 @@
 #!/usr/bin/env bash
-
-#exit if any command fails
-set -e
-
-artifactsFolder="./artifacts"
-
-if [ -d $artifactsFolder ]; then  
-  rm -R $artifactsFolder
-fi
-
-dotnet restore
-
-revision=${TRAVIS_JOB_ID:=1}  
-revision=$(printf "%04d" $revision) 
-
-dotnet pack ./src/System.Data.OracleClient.csproj -c Release -o ./artifacts --version-suffix=$revision  
+dotnet restore && dotnet build
